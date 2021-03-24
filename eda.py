451782759a67512baa77dca19dc8596b945f52b8
kushagra <kushagra2705@gmail.com>
2021-03-24 14:50:32 +0530

    Added eda.py file attempt2

---
+++ eda.py
@@ -0,0 +1,12 @@
+
+# Eda
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+plt.xlabel("x")
+plt.ylabel("X^2")
+plt.title(" x v/s x^2")
+plt.plot([1, 2, 3],[1, 4, 9])
+plt.show()
